1b2b85cac1f852c62aaf906f358abc63793d9f80
Manh Nguyen <manh.nguyen@pfizer.com>
2024-02-20 17:49:51 +0700

    feat: add more endpoints to retrieve popular cars, all cars

--- app/api/index.ts
+++ app/api/index.ts
@@ -3,7 +3,7 @@ const API_BASE_URL = "https://dev-kopm.pantheonsite.io/api";
 export const getCarList = async ({
   page,
 }: {
-  page: number;
+  page: number
 }) => {
   const url = `${API_BASE_URL}/cars?_format=json&page=${page}`;
   const res = await fetch(url, { next: { revalidate: 3600 } });
@@ -17,6 +17,32 @@ export const getCarList = async ({
 
 export const getAllCars = async () => {
   const url = `${API_BASE_URL}/cars/all?_format=json`;
+  const res = await fetch(url, { next: { revalidate: 3600 } });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
+export const getPopularCars = async () => {
+  const url = `${API_BASE_URL}/cars/popular?_format=json`;
+  const res = await fetch(url, { next: { revalidate: 3600 } });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
+export const getCarDetail = async ({
+  id
+}: {
+  id: string
+}) => {
+  const url = `${API_BASE_URL}/car/${id}?_format=json`;
   const res = await fetch(url, { next: { revalidate: 3600 } });
 
   if (!res.ok) {
