7c4d700dc42fb64f5d5b4b26cf21d9e43a835e0e
Manh Nguyen <manh.nguyen@pfizer.com>
2024-02-28 10:31:51 +0700

    feat: handle register flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,48 @@
+import { FaCheckCircle } from "react-icons/fa";
 import { getAllCars, getCarList, getPopularCars } from "./api";
 import CarList from "./components/CarList";
 import HeroBanner from "./components/HeroBanner";
 
-const Home = async () => {
-  const allCars = await getAllCars()
+const Home = async ({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string };
+}) => {
+  const accountActivated = searchParams && searchParams.activated === "1";
+
+  const allCars = await getAllCars();
   const carList = await getCarList({
     page: 0,
   });
-  const numberOfPages = Math.round((allCars.length - carList.length)/4)
+  const numberOfPages = Math.round((allCars.length - carList.length) / 4);
   const popularCars = await getPopularCars();
 
   return (
-    <div>
-      <HeroBanner />
-      <div className="flex flex-col mobile:p-4 p-16">
-        <CarList
-          listTitle="Popular Cars"
-          carItems={popularCars}
-        />
-        <CarList
-          listTitle="All Cars"
-          carItems={carList}
-          numberOfPages={numberOfPages}
-        />
-      </div>
-    </div>
+    <>
+      <main className="w-full bg-gray">
+        {accountActivated && (
+          <section className="message flex gap-4 p-4">
+            <div className="message-header flex justify-center">
+              <FaCheckCircle size={24} style={{ color: "green" }} />
+            </div>
+            <div className="message-content text-center">
+              Your account has been activated! You are now able to log in using
+              your credentials.
+            </div>
+          </section>
+        )}
+        <HeroBanner />
+        <div className="flex flex-col mobile:p-4 p-16">
+          <CarList listTitle="Popular Cars" carItems={popularCars} />
+          <CarList
+            listTitle="All Cars"
+            carItems={carList}
+            numberOfPages={numberOfPages}
+          />
+        </div>
+      </main>
+    </>
   );
-}
+};
 
-export default Home
+export default Home;
