59ffa00193bb3e51873fe90406b42a88c4b9fac9
Manh Nguyen <manh.nguyen@pfizer.com>
2024-02-28 10:10:52 +0700

    feat: add login and register endpoints

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,12 +1,9 @@
+const BASE_URL = "https://dev-kopm.pantheonsite.io";
 const API_BASE_URL = "https://dev-kopm.pantheonsite.io/api";
 
-export const getCarList = async ({
-  page,
-}: {
-  page: number
-}) => {
-  const url = `${API_BASE_URL}/cars?_format=json&page=${page}`;
-  const res = await fetch(url, { next: { revalidate: 3600 } });
+export const getCarList = async ({ page }: { page: number }) => {
+  const endpoint = `${API_BASE_URL}/cars?_format=json&page=${page}`;
+  const res = await fetch(endpoint, { next: { revalidate: 3600 } });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -16,8 +13,8 @@ export const getCarList = async ({
 };
 
 export const getAllCars = async () => {
-  const url = `${API_BASE_URL}/cars/all?_format=json`;
-  const res = await fetch(url, { next: { revalidate: 3600 } });
+  const endpoint = `${API_BASE_URL}/cars/all?_format=json`;
+  const res = await fetch(endpoint, { next: { revalidate: 3600 } });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -27,8 +24,8 @@ export const getAllCars = async () => {
 };
 
 export const getPopularCars = async () => {
-  const url = `${API_BASE_URL}/cars/popular?_format=json`;
-  const res = await fetch(url, { next: { revalidate: 3600 } });
+  const endpoint = `${API_BASE_URL}/cars/popular?_format=json`;
+  const res = await fetch(endpoint, { next: { revalidate: 3600 } });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -37,17 +34,58 @@ export const getPopularCars = async () => {
   return res.json();
 };
 
-export const getCarDetail = async ({
-  id
-}: {
-  id: string
-}) => {
-  const url = `${API_BASE_URL}/car/${id}?_format=json`;
-  const res = await fetch(url, { next: { revalidate: 3600 } });
+export const getCarDetail = async ({ id }: { id: string }) => {
+  const endpoint = `${API_BASE_URL}/car/${id}?_format=json`;
+  const res = await fetch(endpoint, { next: { revalidate: 3600 } });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
+
+  return res.json();
+};
+
+export const userLogIn = async ({
+  username,
+  password,
+}: {
+  username: string;
+  password: string;
+}) => {
+  const endpoint = `${BASE_URL}/user/login?_format=json`;
+
+  const res = await fetch(endpoint, {
+    method: "POST",
+    body: JSON.stringify({ name: username, pass: password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return res.json();
+};
+
+export const userRegister = async ({
+  email,
+  username,
+  password,
+}: {
+  email: string;
+  username: string;
+  password: string;
+}) => {
+  const endpoint = `${BASE_URL}/user/registerpass?_format=json`;
+  const res = await fetch(endpoint, {
+    method: "POST",
+    body: JSON.stringify({
+      name: { value: username },
+      mail: { value: email },
+      pass: { value: password },
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
   return res.json();
 };
