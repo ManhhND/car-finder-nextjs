b3edf6aab5279e575bfb16d8315b1fe354092938
Manh Nguyen <manh.nguyen@pfizer.com>
2024-03-22 14:38:08 +0700

    feat: update Car interface props

--- app/utils/interfaces.ts
+++ app/utils/interfaces.ts
@@ -5,9 +5,9 @@ export interface Car {
   field_car_type: string;
   field_capacity: number;
   field_steering: string;
-  field_gasoline: boolean;
+  field_gasoline: string;
   field_image: string;
-  field_price: number;
+  field_price: string;
   field_brand: string;
   field_favorite: boolean;
   field_popular: string;
